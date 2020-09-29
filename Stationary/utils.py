d32d3599b104b38485f2fb3cbbd8be8775cfb212
alexbergamasco96 <alex.bergamasco@mail.polimi.it>
2020-09-29 11:15:55 +0200

    single input

--- Stationary/utils.py
+++ Stationary/utils.py
@@ -8,11 +8,17 @@ Created on Mon Sep 21 16:38:44 2020
 
 
 import numpy as np
+import math
 
 
 def splitDataset(dataset_X, num_workers, num_rounds):
         
-    x = num_workers * num_rounds
+    a = num_workers * num_rounds
+    
+    b = math.floor(len(dataset_X)/a)
+    
+    x = len(dataset_X) / b
+    
         
     return np.array_split(dataset_X, x)
     
